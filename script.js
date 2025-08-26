ee806329ac5f48bb90fb88e5c3377abc9c53fff3
roksanadilshad <roksanadilshad@gmail.com>
2025-08-27 03:41:19 +0600

    add heart-btn

---
+++ script.js
@@ -0,0 +1,45 @@
+function getNumber(id){
+     const heartCountNumber = parseInt(document.getElementById(id).innerText);
+     return heartCountNumber;
+}
+function getTotal(id, value){
+    const total = document.getElementById(id).innerText = value;
+    return total;
+}
+// document.getElementById('heart-btn').addEventListener('click', function(){
+//     const heartCount = gethearts('heart-count');
+
+//     const totalHeart = heartCount + 1;
+//    getTotal('heart-count', totalHeart)
+
+   
+// })
+ const heartBtns = document.getElementsByClassName('heart-btn');
+
+  for (let btn of heartBtns) {
+    btn.addEventListener('click', function() {
+         const heartCount = getNumber('heart-count');
+
+          const totalHeart = heartCount + 1;
+          getTotal('heart-count', totalHeart)
+    });
+  }
+  
+  document.getElementById('emergency-call').addEventListener('click', function(){
+    const coinCount = getNumber('coin-count');
+
+    const totalCoin = coinCount - 20;
+    
+
+    if(coinCount <= 0 || coinCount < 20){
+        alert("You don't have sufficient 'Coin'");
+        return;
+    }
+    else{
+        alert('📞 Calling National Emergency 999...')
+    }
+    
+        getTotal('coin-count', totalCoin);
+
+   
+})
